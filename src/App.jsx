210528ed5c3f04b82a2e21e422c8170c237cd2b9
Tarak <tarek8295@gmail.com>
2026-07-06 11:06:23 +0600

    chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,73 @@
 import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar"
 import { AppSidebar } from "./components/app-sidebar"
 
+import { Input } from "./components/ui/input"
+
+
+"use client"
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+import { ChartContainer } from "./components/ui/chart"
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+}
+
+export function ChartBarDemoAxis() {
+  return (
+    <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+      <BarChart accessibilityLayer data={chartData}>
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+      </BarChart>
+    </ChartContainer>
+  )
+}
+
+
+
 function App() {
- 
+
 
   return (
-     <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        xjcvh
-      </main>
-    </SidebarProvider>
+
+    <>
+
+      <Input placeholder="Enter text" />
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          Bangladesh
+          <ChartBarDemoAxis />
+        </main>
+      </SidebarProvider>
+    </>
   )
 }
 
